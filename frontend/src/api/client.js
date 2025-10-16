3b031bbc9bf229dcd74c0b8f529e89100c3f3f1a
Sam Greenfield <spgreenf@iu.edu>
2025-10-16 14:55:39 -0400

    add render url

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
-const GRAPHQL_URL = 'http://127.0.0.1:8000/graphql';
+const GRAPHQL_URL = 'https://bloom-3f9y.onrender.com/graphql';
 
 const client = new ApolloClient({
   link: new HttpLink({ uri: GRAPHQL_URL }),
